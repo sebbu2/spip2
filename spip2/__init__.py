79aa66a25002fff017d7a1f3ee3dc77819f270a3
sebbu <zsbe17fr@yahoo.fr>
2020-11-16 23:32:04 +0100

    First commit,  Hello World state

---
+++ spip2/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+# Version of the spip2 package
+__version = "0.0.1"
+
---
+++ spip2/__main__.py
@@ -0,0 +1,13 @@
+# __main__.py
+
+from configparser import ConfigParser
+from importlib import resources  # Python 3.7+
+import sys
+
+def main():
+    """code"""
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+
